2a857b39ea2d4d3ae9326571307ff1f2869ca2f0
NaumanMunir9 <naumanmunir9@gmail.com>
2021-09-25 18:22:33 +0500

    updated the Home

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,10 +1,20 @@
 // libraries
 import { FC } from "react";
 
+// features
+import Diaries from "../diary/Dairies";
+import Editor from "../entry/Editor";
+
 const Home: FC = () => {
   return (
-    <div>
-      <p>Welcome user</p>
+    <div className="two-cols">
+      <div className="left">
+        <Diaries />
+      </div>
+
+      <div className="right">
+        <Editor />
+      </div>
     </div>
   );
 };
